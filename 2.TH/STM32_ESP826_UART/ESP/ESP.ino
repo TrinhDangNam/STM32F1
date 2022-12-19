4dc2961af50da1e0b48bbd914038827367c36c2c
TrinhDangNam <trinhdangnam1501@gmail.com>
2022-12-19 08:29:41 +0700

    18/12/22

---
+++ 2.TH/STM32_ESP826_UART/ESP/ESP.ino
@@ -0,0 +1,26 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(D4, D5); //RX, TX
+// D4 RX - PA9
+// D5 TX - PA10
+// (Send and Receive)
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+
+  if(Serial.available() > 0){//Read from serial monitor and send 
+    String input = Serial.readString();
+    mySerial.println(input);    
+  }
+ 
+  if(mySerial.available() > 1){//Read from  STM module and send to serial monitor
+    String input = mySerial.readString();
+    Serial.println(input);    
+  }
+  delay(20);
+}
